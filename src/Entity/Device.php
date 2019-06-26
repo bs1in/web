d013c2588c9a4069caf457e9b4f0b78dfcbdb06e
sup0823 <susannaprezer@web.de>
2019-06-26 18:56:41 +0200

    Konfiguration Formular

---
+++ src/Entity/Device.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Entity;
+
+class Device
+{
+    protected $id;
+    protected $name;
+    protected $description;
+    protected $attributes;
+    protected $location;
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getDescription() {
+        return $this->description;
+    }
+
+    function getAttributes() {
+        return $this->attributes;
+    }
+
+    function getLocation() {
+        return $this->location;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+    }
+
+    function setDescription($description) {
+        $this->description = $description;
+    }
+
+    function setAttributes($attributes) {
+        $this->attributes = $attributes;
+    }
+
+    function setLocation($location) {
+        $this->location = $location;
+    }
+
+
+}
